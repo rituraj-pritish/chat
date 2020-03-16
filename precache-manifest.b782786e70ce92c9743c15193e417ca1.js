3442c006fe140c52965c3488294f9c106deef2d9
rituraj-pritish <riturajpritish@gmail.com>
2020-03-16 11:28:20 +0530

    Updates

---
+++ precache-manifest.b782786e70ce92c9743c15193e417ca1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be203d46c436bd3c56d20012bdf97818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e121e713aef95224500c",
+    "url": "/static/css/main.2db8a158.chunk.css"
+  },
+  {
+    "revision": "dd28f3d8eb9cf2ad50fd",
+    "url": "/static/js/2.f5a62b82.chunk.js"
+  },
+  {
+    "revision": "6b9f671524e4817f679e05b83b5beb67",
+    "url": "/static/js/2.f5a62b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e121e713aef95224500c",
+    "url": "/static/js/main.675cfdc3.chunk.js"
+  },
+  {
+    "revision": "9fa11034768c57520f9a",
+    "url": "/static/js/runtime-main.da8272fa.js"
+  },
+  {
+    "revision": "632bf4bddeb77fb74430a5ecb33ca9f3",
+    "url": "/static/media/clip-icon.632bf4bd.svg"
+  }
+]);
